e82c5660ba5d12886e32bd4c1cb6454ac20f2721
Miek Gieben <miek@miek.nl>
2015-01-29 15:38:32 +0000

    Move some stuff from server.go to etcd.go

--- server/etcd.go
+++ server/etcd.go
@@ -5,20 +5,26 @@
 package server
 
 import (
+	"encoding/json"
+	"strings"
+
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/miekg/skydns/msg"
 )
 
 // Ectd implementation
 
-type etc etcd.Client
+type etc struct {
+	c *etcd.Client
+}
 
-func (client *etc) Records(path string, recursive bool) []msg.Service {
-	response, err := get(client, path, recursive)
+func (client *etc) Records(path string, recursive bool) ([]msg.Service, error) {
+	response, err := get(client.c, path, recursive)
 	if err != nil {
 		return nil, err
 	}
 	// process them to msg.Service and use those in server.go
+	return nil, nil
 }
 
 // get is a wrapper for client.Get that uses SingleInflight to suppress multiple outstanding queries.
@@ -35,4 +41,90 @@ func get(client *etcd.Client, path string, recursive bool) (*etcd.Response, erro
 	}
 	// shared?
 	return resp, err
+}
+
+type bareService struct {
+	Host     string
+	Port     int
+	Priority int
+	Weight   int
+	Text     string
+}
+
+// skydns/local/skydns/east/staging/web
+// skydns/local/skydns/west/production/web
+//
+// skydns/local/skydns/*/*/web
+// skydns/local/skydns/*/web
+
+// loopNodes recursively loops through the nodes and returns all the values. The nodes' keyname
+// will be match against any wildcards when star is true.
+func loopNodes(n *etcd.Nodes, nameParts []string, star bool, bx map[bareService]bool) (sx []*msg.Service, err error) {
+	if bx == nil {
+		bx = make(map[bareService]bool)
+	}
+Nodes:
+	for _, n := range *n {
+		if n.Dir {
+			nodes, err := s.loopNodes(&n.Nodes, nameParts, star, bx)
+			if err != nil {
+				return nil, err
+			}
+			sx = append(sx, nodes...)
+			continue
+		}
+		if star {
+			keyParts := strings.Split(n.Key, "/")
+			for i, n := range nameParts {
+				if i > len(keyParts)-1 {
+					// name is longer than key
+					continue Nodes
+				}
+				if n == "*" {
+					continue
+				}
+				if keyParts[i] != n {
+					continue Nodes
+				}
+			}
+		}
+		serv := new(msg.Service)
+		if err := json.Unmarshal([]byte(n.Value), serv); err != nil {
+			return nil, err
+		}
+		b := bareService{serv.Host, serv.Port, serv.Priority, serv.Weight, serv.Text}
+		if _, ok := bx[b]; ok {
+			continue
+		}
+		bx[b] = true
+		serv.Ttl = s.calculateTtl(n, serv)
+		if serv.Priority == 0 {
+			serv.Priority = int(s.config.Priority)
+		}
+		serv.Key = n.Key
+		sx = append(sx, serv)
+	}
+	return sx, nil
+}
+
+// calculateTtl returns the smaller of the etcd TTL and the service's
+// TTL. If neither of these are set (have a zero value), the server
+// default is used.
+// need config
+func (s *server) calculateTtl(node *etcd.Node, serv *msg.Service) uint32 {
+	etcdTtl := uint32(node.TTL)
+
+	if etcdTtl == 0 && serv.Ttl == 0 {
+		return s.config.Ttl
+	}
+	if etcdTtl == 0 {
+		return serv.Ttl
+	}
+	if serv.Ttl == 0 {
+		return etcdTtl
+	}
+	if etcdTtl < serv.Ttl {
+		return etcdTtl
+	}
+	return serv.Ttl
 }
